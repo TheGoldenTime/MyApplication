6f97b4ebd2393a0433e4d014e9b2b0c049642e6f
TARUC <marcusloh1018@gmail.com>
2022-07-19 14:36:34 +0800

    Commit 20220719 1435 - Starting up

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.myapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var nameText: EditText
+    lateinit var playerNameTxt: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        nameText = findViewById(R.id.EditPlayerName)
+        playerNameTxt = findViewById(R.id.playerName)
+        val rollBtn:Button = findViewById(R.id.rollButton)
+        rollBtn.setOnClickListener {rollDice()}
+        val updateBtn: Button = findViewById(R.id.updateButton)
+        updateBtn.setOnClickListener {updateName(it)}
+    }
+
+    private fun updateName(view: View){
+        playerNameTxt.text = nameText.text
+
+        nameText.text.clear()
+        nameText.clearFocus()
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
+    }
+
+    private fun rollDice(){
+
     }
 }
